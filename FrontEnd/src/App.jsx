9aec2fb8fbcfcc8ea3cd0d801335fad2d223ab22
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-09-15 14:09:38 +0530

    some error have

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css"
+import Layout from "./layout/Layout"
+function App() {
+
+
+  
+   // return <Layout/>;
+   return (
+      <>
+        <h1 className="text-[20px] text-blue-600">Hello react + vite app</h1>
+      </>
+    );
+}
+
+export default App
