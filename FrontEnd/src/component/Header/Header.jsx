6473a028f1092f364b14baf3675d347a151ad358
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-09-17 20:09:49 +0530

    Doctor Section done

--- FrontEnd/src/component/Header/Header.jsx
+++ FrontEnd/src/component/Header/Header.jsx
@@ -27,6 +27,9 @@ const navLinks = [
   },
 ];
 const  Header = ()=> {
+
+
+  
 const headerRef= useRef(null);
 const menuRef= useRef(null);
 
@@ -52,53 +55,55 @@ return () => window.removeEventListener("scroll", handelStickyHeader)
       <div className="container">
         <div className="flex items-center justify-between">
           <div>
-{/* :::::::::::::::Logo:::::::::::::::::::: */}
+            {/* :::::::::::::::Logo:::::::::::::::::::: */}
             <img src={logo} alt="" />
           </div>
 
-{/* :::::::::::::Navigation::::::::::::: */}
+          {/* :::::::::::::Navigation::::::::::::: */}
 
           <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {navLinks.map((item, index) => (
                 <li key={index}>
-                  
-                    <NavLink
-                      to={item.path}
-                      className={(navClass) =>
-                        navClass.isActive
-                          ? "text-primaryColor text-[16px] leading-7 font-[600]"
-                          : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
-                      }
-                    >
-                      {item.display}
-                    </NavLink>
-                  
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                    }
+                  >
+                    {item.display}
+                  </NavLink>
                 </li>
               ))}
             </ul>
           </div>
-          
-{/* ::::::::::::::::::: Nav_Right ::::::::::::::::::: */}
- 
-          <div className='flex items-center gap-4'>
-            <div className='hidden'>
-              <Link to='/'>
-             <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
-              <img src = {avatar} className='w-full rounded-full alt="" '/>
-               </figure></Link> </div>
 
-               <Link to='/login'>
-               <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] rounded-[50px] flex items-center justify-center '>Login</button>
-               </Link>
+          {/* ::::::::::::::::::: Nav_Right ::::::::::::::::::: */}
 
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img src={avatar} className='w-full rounded-full alt="" ' />
+                </figure>
+              </Link>{" "}
+            </div>
 
-              <span className="w-6 h-6 cursor-pointer md:hidden" onClick={toggleMenu}>
-                <BiMenu className= "w-6 h-6 cursor-pointer" />
-              </span>
-              
-              </div>
-            
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] rounded-[50px] flex items-center justify-center ">
+                Login
+              </button>
+            </Link>
+
+            <span
+              className="w-6 h-6 cursor-pointer md:hidden"
+              onClick={toggleMenu}
+            >
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+          </div>
         </div>
       </div>
     </header>
