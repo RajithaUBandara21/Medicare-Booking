7202d270402620b7e134d5202d41289b0d1495db
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-09-18 16:05:34 +0530

    home page compleat footer also // some hover funtion have error in small diveice it work others not

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -12,9 +12,12 @@ import icon03  from '../assets/images/icon03.png'
 import featureImg  from '../assets/images/feature-img.png'
 import videoIcon  from '../assets/images/video-icon.png'
 import avatarIcon  from '../assets/images/avatar-icon.png'
+import faqImg  from '../assets/images/faq-img.png'
 
 import ServiceList from '../component/Services/ServiceList'
 import DoctorList from '../component/Doctors/DoctorList'
+import FaqList from '../component/Faq/FaqList'
+import Testimonial from '../component/Testimonial/Testimonial'
 
 
 
@@ -177,7 +180,7 @@ const Home = () => {
           </section>
           {/* ::::::::::::::;About::::::::::::::: */}
           <About />
-
+          {/* :::::::::::::Services::::::::::::::: */}
           <section>
             <div className="container">
               <div className="xl:w-[470px] mx-auto">
@@ -191,7 +194,6 @@ const Home = () => {
               </div>
             </div>
           </section>
-
           <section>
             <ServiceList />
           </section>
@@ -241,35 +243,75 @@ const Home = () => {
                       </span>
                     </div>
 
-                    <div className='w-[65px] lg:w-[90px] bg-[#ccf0f3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full'>
+                    <div className="w-[65px] lg:w-[90px] bg-[#ccf0f3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
                       Consultation
                     </div>
-                    <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px] '>
-                      <img src= {avatarIcon} alt=''  />
-                      <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>Bru bru</h4>'
-
+                    <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px] ">
+                      <img src={avatarIcon} alt="" />
+                      <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                        Bru bru
+                      </h4>
+                      '
                     </div>
-
                   </div>
                 </div>
               </div>
             </div>
           </section>
 
+          {/* ::::::::::::::::Doctors:::::::::::::::: */}
+          <section>
+            <div className="container">
+              <div className="lg:w-[470px] mx-auto">
+                <h2 className="heading text-center">Our Great Doctors</h2>
+                <p className="text_para text-center">
+                  Our team of experienced and highly skilled doctors are
+                  dedicated to providing exceptional medical care. With years of
+                  expertise and a commitment to patient well-being, our doctors
+                  strive to deliver personalized treatment plans and ensure the
+                  best possible outcomes for our patients. Whether you need a
+                  routine check-up or specialized care, our great doctors are
+                  here to help you on your journey to better health. Trust in
+                  our expertise and experience for all your medical needs.
+                </p>
+              </div>
+              <DoctorList />
+            </div>
+          </section>
 
+          {/* ::::::::faq section:::::::::::::::; */}
+          <section>
+            <div className="container">
+              <div className="flex justify-between gap-[50px] lg:gap-0">
+                <div className="w-1/2  md:block">
+                  <img src={faqImg} alt="" />
+                </div>
+
+                <div className='w-full md:w-1/2'>
+                <h2 className='heading'> Most questions by your beloved patients</h2>
+                  <FaqList/>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          {/* ::::::::::test imoinal:::::::::::::::: */}
+
+          {/* Not Working pl check */}
           <section>
             <div className="container">
             <div className="lg:w-[470px] mx-auto">
-                <h2 className="heading text-center">
-              Our Great Doctors
-                </h2>
+                <h2 className="heading text-center">What our patient say</h2>
                 <p className="text_para text-center">
-                Our team of experienced and highly skilled doctors are dedicated to providing exceptional medical care. With years of expertise and a commitment to patient well-being, our doctors strive to deliver personalized treatment plans and ensure the best possible outcomes for our patients. Whether you need a routine check-up or specialized care, our great doctors are here to help you on your journey to better health. Trust in our expertise and experience for all your medical needs.
+                Our patients' satisfaction is our top priority. Here are some of the testimonials from our beloved patients who have experienced our exceptional care and services. We are grateful for their kind words and trust in our medical team.
                 </p>
               </div>
-              <DoctorList/>
+              <Testimonial/>
             </div>
           </section>
+
+
+
         </>
       );
 }
