e50c5e15ec1257b8b96afb7a7d12835f7642d185
RajithaUBandara21 <138967884+RajithaUBandara21@users.noreply.github.com>
2024-09-19 19:53:03 +0530

    sing in login pages done

--- FrontEnd/src/pages/Contact.jsx
+++ FrontEnd/src/pages/Contact.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+   <section>
+    <div className="px-4 mx-auto max-w-screen-md">
+    <h2 className="heading text-center ">Contact Us</h2>
+    < p className='mb-8 lg:mb-16 font-light text-center text_para'>
+    If you have any questions or need further information, feel free to contact us. Our team is here to help you.
+    </p>
+
+    <form action='#' className='space-y-8'>
+      <div>
+        <label htmlFor='email' className='form_label'>
+          Your Email  
+        </label>
+        <input
+        type='email'
+        id='email'
+        placeholder='example@gmail.com'
+        className='form_input mt-1'/> 
+      </div>
+
+      <div>
+        <label htmlFor='subject' className='form_label'>
+       Subject
+        </label>
+        <input
+        type='text'
+        id='subject'
+        placeholder='Let us know how we can help you'
+        className='form_input mt-1'/> 
+
+      </div>
+
+      <div className='sm:col-span-2'>
+        <label htmlFor='message' className='form_label'>
+         Your message  
+        </label>
+        <textarea
+        rows='6'
+        type='text'
+        id='message'
+        placeholder='Leave hear'
+        className='form_input mt-1'/> 
+      </div>
+      <button className='btn rounded sm:w-fit' type='submit'>
+        Send
+      </button>
+    </form>
     </div>
+   </section>
   )
 }
 
--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [fromData,setFromData] = useState({
+    email:'',
+    password:'',
+  });
+
+  const handeleInputChange = e => {
+    setFromData({...FormData,[e.target.name]:e.target.value});
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <section className="px-5 lg:px-0">
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10 ">
+        <h3 className="text-headingColor text-[16px] leading-9 font-bold mb-10">
+          Hello!  <span className="text-primaryColor"> Welcome</span>back😊
+        </h3>
+
+        <form className="py-5  px-6 md:py-0">
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={FormData.email}
+              onChange={handeleInputChange}
+              className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
+          focus:border-b-primaryColor text-[22px] focus:shadow-xl leading-7 text-headingColor placeholder:text-textColor round-md cursor-pointer"
+              required
+            />
+          </div>
+
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={FormData.password}
+              onChange={handeleInputChange}
+              className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none
+          focus:border-b-primaryColor focus:shadow-xl  text-[22px] leading-7 text-headingColor placeholder:text-textColor round-md cursor-pointer"
+              required
+            />
+          </div>
+          <div className="mt-7">
+            <button
+            type = "submit"
+            className='w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 cursor-pointer'>
+              Login
+            </button>
+          </div>
+          <p className='mt-5 text-textColor text-center'>
+            Don't have an account? <Link to='/register' className='text-primaryColor cursor-pointer'>Sign Up</Link>
+          </p>
+
+
+
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Login
